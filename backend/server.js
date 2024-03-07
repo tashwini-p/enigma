1981aed05eb4d4e1e4013e284a8574f947af9f44
tashwini-p <tashwiniprakasha@gmail.com>
2024-03-08 00:53:14 +0530

    server.js updated

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,12 @@
 const jsonServer = require("json-server");
 const server = jsonServer.create();
 const router = jsonServer.router("db.json");
+const middlewares = jsonServer.defaults();
 
 server.db = router.db;
 
+// Set up the authentication middleware
+server.use(middlewares);
 server.use(router);
 
 const port = process.env.PORT || 4000;
